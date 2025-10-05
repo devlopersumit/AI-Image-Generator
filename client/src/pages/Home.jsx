ffa0675449a40015d02dfd28c675873d13e9eec1
devlopersumit <sumitsj2002@gmail.com>
2025-10-05 22:38:31 +0530

    setup routing, created header and main component

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <h1 className="text-5xl font-extrabold underline text-blue-600">AI Image Generator</h1>
+    </div>
+  )
+}
+
+export default Home
